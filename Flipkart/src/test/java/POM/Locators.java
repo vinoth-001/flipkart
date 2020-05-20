a6b3db031a3ad5ab76ebef71672af037abbb596d
lenovo pc <lenovo pc@LAPTOP-GBFKP4U7>
2020-05-20 20:07:19 +0530

    initial commit

--- Flipkart/src/test/java/POM/Locators.java
+++ Flipkart/src/test/java/POM/Locators.java
@@ -1,16 +1,20 @@
 package POM;
 
+import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Properties;
 
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Locators {
-	WebDriver driver1 = new ChromeDriver ();
+
 	WebDriver driver = null;
 
 	public Locators(WebDriver driver) {
@@ -22,21 +26,38 @@ public class Locators {
 	By pwd = By.xpath("//input[@class='_2zrpKA _3v41xv _1dBPDZ']");
 	By login = By.xpath("//button[@class='_2AkmmA _1LctnI _7UHT_c']");
 
-	public void singIn() throws IOException {
+	public String getExcelData(int rowNum, int colNum) throws IOException {
+		File f = new File("D:\\Book1.xlsx");
+		FileInputStream fIS = new FileInputStream(f);
+
+		XSSFWorkbook wB = new XSSFWorkbook(fIS);
+		XSSFSheet sheet = wB.getSheet("sheet1");
+		String data = sheet.getRow(rowNum).getCell(colNum).getStringCellValue();
+		return data;
+
+	}
+
+	public static Properties readPropertiesFiles(String fileName) throws IOException {
 		FileInputStream file = new FileInputStream("confiq.properties");
 		Properties prop = new Properties();
 		prop.load(file);
-		driver.findElement(uname).sendKeys(prop.getProperty("uname"));
-		driver.findElement(pwd).sendKeys(prop.getProperty("pwd"));
+		return prop;
+	}
+
+	public void singIn() throws IOException {
+		Properties prop = readPropertiesFiles("confiq.properties");
+		driver.findElement(uname).sendKeys(prop.getProperty("username"));
+		driver.findElement(pwd).sendKeys(prop.getProperty("password"));
+		// driver.findElement(uname).sendKeys(l.getExcelData(0, 2));
+		// driver.findElement(pwd).sendKeys(l.getExcelData(0, 3));
 		driver.findElement(login).click();
 	}
 
 	public void do_search() throws IOException {
-		FileInputStream file = new FileInputStream("confiq.properties");
-		Properties prop = new Properties();
-		prop.load(file);
+		Properties prop = readPropertiesFiles("confiq.properties");
 		driver.findElement(search).sendKeys(prop.getProperty("product"));
+		// driver.findElement(search).sendKeys(l.getExcelData(0, 1));
 		driver.findElement(search).sendKeys(Keys.ENTER);
-		driver.findElement(By.xpath("(//div[@class='_1p7h2j'])[2]")).click();
+
 	}
 }
